364d2ae423bc6302e9f3d3ddc9ad205d92b7a9bc
kenshien <kenshienworkacc@gmail.com>
2026-05-17 00:49:27 +0800

    chore: update offline cache storage key to V1 version

--- src/provider/query-provider.tsx
+++ src/provider/query-provider.tsx
@@ -25,7 +25,7 @@ export default function QueryProvider({ children }: { children: ReactNode }) {
     if (typeof window !== "undefined") {
       const p = createSyncStoragePersister({
         storage: window.localStorage,
-        key: 'CISM_CLIENT_OFFLINE_CACHE',
+        key: 'CISM_OFFLINE_CACHE_V1',
       });
       setPersister(p);
     }
